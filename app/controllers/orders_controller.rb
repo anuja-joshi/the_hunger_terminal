e159eccea714f1c6ae57c411f8d17d67adb33dc7
Anuja Joshi <anuja.joshi@joshsoftware.com>
2017-03-09 21:58:02 +0530

    [refs #55] display and select menuitems

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
   def new
     @current_user = User.last
+    @order        = Order.new
     terminals_ids = Terminal.where(active: true, company: @current_user.company).ids
     @menu_items = MenuItem.where(terminal_id: terminals_ids, visibility: true)
+  end
+
+  def create
+    p params
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_details
   belongs_to :user
+
+  accepts_nested_attributes_for :order_details
 end
