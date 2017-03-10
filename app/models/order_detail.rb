5d9321fcba5c049473ce8af82a3403a99a22c415
Anuja Joshi <anuja.joshi@joshsoftware.com>
2017-03-10 11:35:14 +0530

    [refs #55] Create Order

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,8 @@
 class OrderDetail < ApplicationRecord
-  belongs_to :order
+  belongs_to :order, optional: true
   belongs_to :terminal
+  belongs_to :menu_item
+  after_initialize :assign_menu_item_details, if: :menu_item
 
   def self.daily_orders
     self.
@@ -9,5 +11,13 @@ class OrderDetail < ApplicationRecord
       group(:menu_item_name, "terminals.name, terminals.id").
       select("sum(quantity) as total_quantity, menu_item_name,
         terminals.name as terminal_name, terminals.id as t_id")
+  end
+
+  def assign_menu_item_details
+    self.menu_item_name = menu_item.name
+    self.menu_item_price = menu_item.price
+    # veg = 
+    self.terminal_name = menu_item.terminal.name
+    self.terminal = menu_item.terminal
   end
 end
